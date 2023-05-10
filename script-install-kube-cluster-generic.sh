870948097753db776390ab71d4a56c146b0edbce
Ciro Bessa <60109116+cirobessa@users.noreply.github.com>
2023-05-10 14:04:35 -0300

    Update script-install-kube-cluster-generic.sh

--- script-install-kube-cluster-generic.sh
+++ script-install-kube-cluster-generic.sh
@@ -124,7 +124,12 @@ echo additional settings
 echo "
   mkdir -p $HOME/.kube
   sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-  sudo chown $(id -u):$(id -g) $HOME/.kube/config"  >> /etc/profile
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config"  >> /etc/skel/.bash_profile 
+  
+  echo "
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config"  >> ~/.bash_profile 
 #
 echo Install the Calico network add-on
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
